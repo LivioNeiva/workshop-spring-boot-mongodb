1568a6a7939ad49f80ac74921a601af03a2fffc7
Livio Neiva <livio_neiva@hotmail.com>
2021-04-05 03:09:03 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/main/java/com/livioneiva/workshopmongo/WorkShopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.livioneiva.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livioneiva/workshopmongo/WorkShopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livioneiva.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
